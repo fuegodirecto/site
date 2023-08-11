893ba27920f65a90732675d0916aa49b935dcb56
afuegodirecto <entutarget+fuegodirecto@gmail.com>
2023-08-11 15:58:46 -0600

    Se modifico la URL base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://afuegodirecto.github.io',
-  base: '/a-fuego-directo',
+  base: '/a-fuego-directo-site',
 
 });
